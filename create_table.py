12b4b39cfbd6c872162d4f8b7010eb3c6f835053
brianbrosky <64482736+brianbrosky@users.noreply.github.com>
2023-04-29 19:57:51 -0300

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -1,9 +1,10 @@
 import psycopg2
-dbname = "database-1"
-user = "postgres" #Configuracion / Disponibilidad / nombre de usuario maestro
-password = "chavoLOCO23"
-host = "database-1.c7mkdwca7kj0.us-east-1.rds.amazonaws.com" #Econectividad y seguridad
+dbname = "database"
+user = "modelos" #Configuracion / Disponibilidad / nombre de usuario maestro
+password = "Chavoloco23"
+host = "database.crv8bjyoa2v8.us-east-1.rds.amazonaws.com" #Econectividad y seguridad
 port = "5432"
+
 
 #Creamos la conexión a RDS
 conn = psycopg2.connect(
@@ -15,7 +16,9 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
-cur.execute('CREATE TABLE tabla_RDS (adv_id string)')
+cur.execute('create table top_20_ctr (adv_id string, product_id string, cantidad int, fecha_recom string)')
+cur.execute('create table top_20 (adv_id string, product_id string, click int, impression int, click-through-rate float, fecha_recom string)')
+
 conn.commit()
 
 # Cerrar la conexión
