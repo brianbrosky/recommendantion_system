e5945b3fc95853fa9355eafa74c45e48ae06255b
Brian <brianbrosky@gmail.com>
2023-04-29 16:53:12 -0300

    cambios prueba.py 3

--- prueba.py
+++ prueba.py
@@ -2,6 +2,9 @@ import datetime
 import pandas as pd
 import boto3
 
+s3 = boto3.client("s3") #definimos un cliente para trabajar con S3 usando boto3
+bucket_name = "udesa-tp" #el nombre de nuestro bucket creado
+
 s3_object_advertiser_ids = "/Data/Raw/advertiser_ids.csv" #el archivo que vamos a traernos
 s3_object_ads_views = "/Data/Raw/ads_views.csv" #el archivo que vamos a traernos
 s3_object_product_views = "/Data/Raw/product_views.csv" #el archivo que vamos a traernos
