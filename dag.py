5366e83a010a476029441c95259ac8297d2a8685
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-05-05 00:15:43 -0300

    Update dag.py

--- dag.py
+++ dag.py
@@ -183,11 +183,11 @@ def DBWriting(s3_object_df_top20, s3_object_df_top20_CTR):
 
 #Definimos nuestro DAG y sus tareas.
 with DAG(
-#     dag_id = 'Recomendar',
-#     schedule_interval= '0 0 * * *', #se ejecuta a las 00:00 todos los días, todas las semanas, todos los meses
-#     start_date=datetime.datetime(2022,4,1),
-#     catchup=False
-# ) as dag:
+    dag_id = 'Recomendar',
+    schedule_interval= '0 0 * * *', #se ejecuta a las 00:00 todos los días, todas las semanas, todos los meses
+    start_date=datetime.datetime(2022,4,1),
+    catchup=False
+) as dag:
 #     FiltrarDatos = PythonOperator(
 #         task_id='Filtro',
 #         python_callable=FiltrarDatos, #función definida arriba
