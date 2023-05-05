0958e2a65f2d51dc066f32850c34ea7e828b3853
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-05-05 20:26:41 -0300

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -20,9 +20,9 @@ cur = conn.cursor()
 #cur.execute('create table top_20 (adv_id varchar(50), product_id varchar(50), click int, impression int, clickthroughrate float(3), fecha_recom timestamp)')
 #cur.execute('alter table top_20 alter column fecha_recom type timestamp without time zone using fecha_recom::timestamp without time zone')
 #cur.execute('alter table top_20 alter column fecha_recom type timestamp without time zone using fecha_recom::timestamp without time zone')
-cur.execute("select column_name, data_type from information_schema.columns where table_name = 'top_20_ctr'")
-for column_name, data_type in cur.fetchall():
-    print(column_name, data_type)
+# cur.execute("select column_name, data_type from information_schema.columns where table_name = 'top_20_ctr'")
+# for column_name, data_type in cur.fetchall():
+#     print(column_name, data_type)
 
 
 conn.commit()
