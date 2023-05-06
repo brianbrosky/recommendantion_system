bc299000c52663d1b5187d33e3223e82aa584342
patricioaubone <131826145+patricioaubone@users.noreply.github.com>
2023-05-06 12:00:27 -0300

    Update create_table.py

--- create_table.py
+++ create_table.py
@@ -16,20 +16,31 @@ conn = psycopg2.connect(
 )
 
 cur = conn.cursor()
+#CREACION
 #cur.execute('create table top_20 (adv_id varchar(50), product_id varchar(50), cantidad int, fecha_recom varchar(50))')
 #cur.execute('create table top_20_ctr (adv_id varchar(50), product_id varchar(50), click int, impression int, clickthroughrate float(3), fecha_recom varchar(50))')
+
+#MODIFICAR TIPO DE DATO
 # cur.execute('alter table top_20 alter column fecha_recom type varchar(50)')
 # cur.execute('alter table top_20_ctr alter column fecha_recom type varchar(50)')
 
+#CONSULTAR EL TIPO DE DATO DE CADA CAMPO
 # cur.execute("select column_name, data_type from information_schema.columns where table_name = 'top_20'")
 # for column_name, data_type in cur.fetchall():
 #     print(column_name, data_type)
 
-cur.execute('select * from top_20') 
+
+#BORRAR REGISTROS
+cur.execute('delete from top_20_ctr')
+
+#SELECT PARA VER REGISTROS
+cur.execute('select * from top_20_ctr') 
 rows = cur.fetchall()
 for row in rows:
     print(row)
-
+    
+    
+    
 conn.commit()
 
 # Cerrar la conexión
